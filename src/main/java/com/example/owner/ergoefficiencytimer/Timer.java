440ad0b0b15738347c5da3128043c2d6f57450b1
Kushal <kushal.k@hotmail.com>
2016-04-29 20:08:18 -0400

    added timer class which will handle most of the logic. Also made simple UI changes, background changes color

--- src/main/java/com/example/owner/ergoefficiencytimer/Timer.java
+++ src/main/java/com/example/owner/ergoefficiencytimer/Timer.java
@@ -1,8 +1,11 @@
 package com.example.owner.ergoefficiencytimer;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Handler;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -11,7 +14,18 @@ import android.widget.Toast;
 public class Timer {
     boolean timerOn = false;
     Runnable runable;
-    long timeRemaining;
+    long currentTimeRemaining;
+    long overallTimeRemaining;
+
+    int shortBreakLength;
+    int longBreakLength;
+    int sprintLength;
+    int longBreakFrequency;
+    int longBreakCountDown;
+    boolean inSprint;
+
+
+
     Handler handler;
     Activity activity;
 
@@ -20,33 +34,97 @@ public class Timer {
 
         this.handler = new Handler();
 
+        this.shortBreakLength = 5;
+        this.longBreakLength = 10;
+        this.sprintLength = 10;
+        this.longBreakFrequency = 3;
+        this.longBreakCountDown = this.longBreakFrequency;
+        this.inSprint = true;
+
+        this.currentTimeRemaining = this.sprintLength;
+
+
         this.runable = new Runnable(){
             @Override
             public void run() {
              //   Log.d("KUSH", "HELLO from the handler");
 
 
-                timeRemaining -= 1;
-                if(timeRemaining > 0 && timerOn){
-                    EditText timeText = (EditText) activity.findViewById(R.id.timeText);
 
-                    timeText.setText(""+timeRemaining);
-                    handler.postDelayed(this, 1000);
+                if(overallTimeRemaining > 0 && timerOn){
+
+
+
+                    if(currentTimeRemaining > 0){
+                        EditText timeText = (EditText) activity.findViewById(R.id.timeText);
+                        timeText.setText(""+currentTimeRemaining);
+                        handler.postDelayed(this, 1000);
+                    } else if(!inSprint){
+                        System.out.println("Sprint time!");
+
+                        inSprint = !inSprint;
+                        currentTimeRemaining = sprintLength;
+
+                        TextView state = (TextView) activity.findViewById(R.id.stateText);
+                        state.setText("Sprint");
+                        RelativeLayout root = (RelativeLayout) activity.findViewById(R.id.rootLayout);
+                        root.setBackgroundColor(Color.parseColor("#FF6666"));
+
+
+                        EditText timeText = (EditText) activity.findViewById(R.id.timeText);
+                        timeText.setText(""+currentTimeRemaining);
+                        handler.postDelayed(this, 1000);
+                    } else if(longBreakCountDown == 0){
+                        System.out.println("long break time!!");
+
+                        longBreakCountDown = longBreakFrequency;
+                        currentTimeRemaining = longBreakLength;
+
+                        TextView state = (TextView) activity.findViewById(R.id.stateText);
+                        state.setText("Long Break");
+                        RelativeLayout root = (RelativeLayout) activity.findViewById(R.id.rootLayout);
+                        root.setBackgroundColor(Color.parseColor("#B2B2FF"));
+
+                        EditText timeText = (EditText) activity.findViewById(R.id.timeText);
+                        timeText.setText(""+currentTimeRemaining);
+                        handler.postDelayed(this, 1000);
+                    } else if(inSprint){
+                        System.out.println("short break time!!!!");
+                        inSprint = !inSprint;
+                        longBreakCountDown--;
+                        currentTimeRemaining = shortBreakLength;
+
+                        TextView state = (TextView) activity.findViewById(R.id.stateText);
+                        state.setText("Short Break");
+                        RelativeLayout root = (RelativeLayout) activity.findViewById(R.id.rootLayout);
+                        root.setBackgroundColor(Color.parseColor("#B2B2FF"));
+
+                        EditText timeText = (EditText) activity.findViewById(R.id.timeText);
+                        timeText.setText(""+currentTimeRemaining);
+                        handler.postDelayed(this, 1000);
+                    } else {
+                        System.out.println("ERRRRRRROOOOOORRRRR!!!!");
+                    }
+
+                    currentTimeRemaining -= 1;
 
                     //           int time = Integer.parseInt(timeText.getText().toString());
 
                 }
-
+                overallTimeRemaining -= 1;
             }
+
+
         };
     }
 
-    public void setTimeRemaining(int timeRemaining) {
-        this.timeRemaining = timeRemaining;
+
+    public void setOverallTimeRemaining(int timeRemaining) {
+        this.overallTimeRemaining = timeRemaining;
     }
 
-    public void setTimeRemaining(long timeRemaining) {
-        this.timeRemaining = timeRemaining;
+    public void setOverallTimeRemaining(long timeRemaining) {
+        this.overallTimeRemaining = timeRemaining;
     }
 
     public Runnable getRunable() {
@@ -58,7 +136,7 @@ public class Timer {
             EditText timeText = (EditText) activity.findViewById(R.id.timeText);
 
             int time = Integer.parseInt(timeText.getText().toString());
-            this.setTimeRemaining(time);
+            this.setOverallTimeRemaining(time);
 
             Toast.makeText(activity, "ON", Toast.LENGTH_SHORT).show();
             timerOn = true;
